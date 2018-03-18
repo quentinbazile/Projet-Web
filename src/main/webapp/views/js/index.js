819e58fd1a1fa25c3a9c0327436f167556ae2afb
Quentin <Quentin@192.168.1.30>
2018-03-18 19:42:21 +0100

    js views

---
+++ src/main/webapp/views/js/index.js
@@ -0,0 +1,64 @@
+$(document).ready(// Exécuté à la fin du chargement de la page
+        function () {
+            // On montre la liste des produits et des commandes
+            showProducts();
+            showOrders();
+        }
+);
+
+function showProducts() {
+    // On fait un appel AJAX pour chercher les produits
+    $.ajax({
+        url: "listeProduits",
+        dataType: "json",
+        error: showError,
+        success: // La fonction qui traite les résultats
+                function (result) {
+                    // Le code source du template est dans la page
+                    var template = $('#productsTemplate').html();
+                    // On combine le template avec le résultat de la requête
+                    var processedTemplate = Mustache.to_html(template, result);
+                    // On affiche la liste des options dans le select
+                    $('#products').html(processedTemplate);
+                }
+    });
+}
+
+function showOrders() {
+    // On fait un appel AJAX pour chercher les produits
+    $.ajax({
+        url: "listeCommandes",
+        dataType: "json",
+        error: showError,
+        success: // La fonction qui traite les résultats
+                function (result) {
+                    // Le code source du template est dans la page
+                    var template = $('#ordersTemplate').html();
+                    // On combine le template avec le résultat de la requête
+                    var processedTemplate = Mustache.to_html(template, result);
+                    // On affiche la liste des options dans le select
+                    $('#orders').html(processedTemplate);
+                }
+    });
+}
+
+// Ajouter une commande
+function addCommande(product_id) {
+    $.ajax({
+        url: "addCommande",
+        data: {"product_id": product_id},
+        dataType: "json",
+        success: // La fonction qui traite les résultats
+                function (result) {
+                    showOrders();
+                    console.log(result);
+                },
+        error: showError
+    });
+    return false;
+}
+
+// Fonction qui traite les erreurs de la requête
+function showError(xhr, status, message) {
+    alert(JSON.parse(xhr.responseText).message);
+}
