f27a208c1c5322589c49985309271f20ab8df120
Quentin <Quentin@192.168.1.30>
2018-04-10 14:22:23 +0200

    quelques commentaires...

--- src/main/java/controllers/JsonAddCommandeController.java
+++ src/main/java/controllers/JsonAddCommandeController.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Date;
-import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,98 +17,93 @@ import modeles.DAO;
 import modeles.DAOException;
 import modeles.DataSourceFactory;
 
-/**
- *
- * @author rbastide
- */
-@WebServlet(name = "addCommande", urlPatterns = {"/addCommande"})
+@WebServlet(name = "addCommande", urlPatterns = {"/addCommande"}) // L'URL de la Servlet
 public class JsonAddCommandeController extends HttpServlet {
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException, DAOException {
-            
-		DAO dao = new DAO(DataSourceFactory.getDataSource());
-                
-                int quantity = Integer.parseInt(request.getParameter("qte"));
-                int product_id = Integer.parseInt(request.getParameter("product_id"));
-                String freight_company = request.getParameter("fc");
-		String message;
-                
-		int order_num = dao.orderNum() + 1;
-		int customer_id = LoginController.passwordParam;
-                float shipping_cost = 400 + 2 * quantity;
-                Date sales_date = new Date(System.currentTimeMillis());
-                Date shipping_date = sales_date;
-		
-		dao.ajoutCommande(order_num, customer_id, product_id, quantity, shipping_cost, sales_date, shipping_date, freight_company);
-                dao.updateQuantity(quantity, product_id);
-                message = String.format("Commande %s ajoutée", order_num);
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws modeles.DAOException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, DAOException {
 
-		Properties resultat = new Properties();
-		resultat.put("message", message);
+        DAO dao = new DAO(DataSourceFactory.getDataSource());
 
-		try (PrintWriter out = response.getWriter()) {
-			// On spécifie que la servlet va générer du JSON
-			response.setContentType("application/json;charset=UTF-8");
-			// Générer du JSON
-			Gson gson = new Gson();
-			out.println(gson.toJson(resultat));
-		}
-	}
+        // On récupère les paramètres saisis par le client
+        int quantity = Integer.parseInt(request.getParameter("qte"));
+        int product_id = Integer.parseInt(request.getParameter("product_id"));
+        String freight_company = request.getParameter("fc");
+        String message;
+
+        // On définit les autres variables par rapport aux éléments saisis
+        int order_num = dao.orderNum() + 1;
+        int customer_id = LoginController.passwordParam;
+        float shipping_cost = 400 + 2 * quantity;
+        Date sales_date = new Date(System.currentTimeMillis());
+        Date shipping_date = sales_date;
+
+        // On exécute les requêtes du DAO
+        dao.ajoutCommande(order_num, customer_id, product_id, quantity, shipping_cost, sales_date, shipping_date, freight_company);
+        dao.updateQuantity(quantity, product_id);
+        message = String.format("Commande %s ajoutée", order_num);
+
+        Properties resultat = new Properties();
+        resultat.put("message", message);
+
+        try (PrintWriter out = response.getWriter()) {
+            // On spécifie que la servlet va générer du JSON
+            response.setContentType("application/json;charset=UTF-8");
+            // Générer du JSON
+            Gson gson = new Gson();
+            out.println(gson.toJson(resultat));
+        }
+    }
 
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (DAOException ex) {
-                Logger.getLogger(JsonAddCommandeController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOException ex) {
+            Logger.getLogger(JsonAddCommandeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (DAOException ex) {
-                Logger.getLogger(JsonAddCommandeController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOException ex) {
+            Logger.getLogger(JsonAddCommandeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
-
+    /**
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    } // </editor-fold>
 }
--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -13,143 +13,143 @@ import modeles.DAOException;
 import modeles.DataSourceFactory;
 
 public class LoginController extends HttpServlet {
-    
+
     protected static String userName, Admin;
     protected static int passwordParam;
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-         * @throws modeles.DAOException
-	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException, DAOException {
-		// Quelle action a appelé cette servlet ?
-		String action = request.getParameter("action");
-		if (null != action) {
-			switch (action) {
-				case "Connexion":
-					checkLogin(request);
-					break;
-				case "DECONNEXION":
-					doLogout(request);
-					break;
-			}
-		}
-
-		// Est-ce que l'utilisateur est connecté ?
-		// On cherche l'attribut userName dans la session
-		userName = findUserInSession(request);
-                Admin = findAdminInSession(request);
-		String jspView;
-		if (null == userName) { // L'utilisateur n'est pas connecté
-			// On choisit la page de login
-			jspView = "views/login.jsp";
-
-		} else { // L'utilisateur est connecté
-			// On choisit la page d'affichage
-                        if(userName.equals(Admin))
-                            jspView = "views/index_admin.html";
-                        else   
-                            jspView = "views/index.html";
-		}
-		// On va vers la page choisie
-		request.getRequestDispatcher(jspView).forward(request, response);
-
-	}
-
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (DAOException ex) {
-                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws modeles.DAOException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, DAOException {
+        // Quelle action a appelé cette servlet ?
+        String action = request.getParameter("action");
+        if (null != action) {
+            switch (action) {
+                case "Connexion":
+                    checkLogin(request);
+                    break;
+                case "DECONNEXION":
+                    doLogout(request);
+                    break;
             }
-	}
+        }
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (DAOException ex) {
-                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        // Est-ce que l'utilisateur est connecté ?
+        // On cherche les attributs userName et Admin dans la session
+        userName = findUserInSession(request);
+        Admin = findAdminInSession(request);
+        String jspView;
+        if (null == userName) { // L'utilisateur n'est pas connecté
+            // On choisit la page de login
+            jspView = "views/login.jsp";
+
+        } else { // L'utilisateur est connecté
+            // On choisit la page d'affichage
+            if (userName.equals(Admin)) {
+                jspView = "views/index_admin.html";
+            } else {
+                jspView = "views/index.html";
             }
-	}
+        }
+        // On va vers la page choisie
+        request.getRequestDispatcher(jspView).forward(request, response);
+    }
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	private void checkLogin(HttpServletRequest request) throws DAOException {
-		// Les paramètres transmis dans la requête
-		String loginParam = request.getParameter("loginParam");
-                String passwordParamAdmin = request.getParameter("passwordParam");
-                try {
-                    passwordParam = Integer.parseInt(request.getParameter("passwordParam"));
-                }
-                catch(NumberFormatException nfe){ 
-                    passwordParam = 0;
-                }
-                
-                // Le login/password défini dans web.xml
-		String login = getInitParameter("login");
-		String password = getInitParameter("password");
-   
-                DAO dao = new DAO(DataSourceFactory.getDataSource());
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-		if (dao.checkLogin(loginParam, passwordParam) || (login.equals(loginParam) && (password.equals(passwordParamAdmin)))) {
-			// On a trouvé la combinaison login / password
-			// On stocke l'information dans la session
-			HttpSession session = request.getSession(true); // démarre la session
-			session.setAttribute("userName", loginParam);
-                        session.setAttribute("Admin", login);
-		}
-	}
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
-	private void doLogout(HttpServletRequest request) {
-		// On termine la session
-		HttpSession session = request.getSession(false);
-		if (session != null) {
-			session.invalidate();
-		}
-	}
+    private void checkLogin(HttpServletRequest request) throws DAOException {
+        // Les paramètres transmis dans la requête
+        String loginParam = request.getParameter("loginParam");
+        String passwordParamAdmin = request.getParameter("passwordParam");
+        try {
+            passwordParam = Integer.parseInt(request.getParameter("passwordParam"));
+        } catch (NumberFormatException nfe) {
+            passwordParam = 0;
+        }
 
-	private String findUserInSession(HttpServletRequest request) {
-		HttpSession session = request.getSession(false);
-		return (session == null) ? null : (String) session.getAttribute("userName");
-	}
-        
-        private String findAdminInSession(HttpServletRequest request) {
-		HttpSession session = request.getSession(false);
-		return (session == null) ? null : (String) session.getAttribute("Admin");
-	}
+        // Le login/password défini dans web.xml pour l'administrateur
+        String login = getInitParameter("login");
+        String password = getInitParameter("password");
+
+        DAO dao = new DAO(DataSourceFactory.getDataSource());
+
+        if (dao.checkLogin(loginParam, passwordParam) || (login.equals(loginParam) && (password.equals(passwordParamAdmin)))) {
+            // On a trouvé la combinaison login / password
+            // On stocke l'information dans la session
+            HttpSession session = request.getSession(true); // démarre la session
+            session.setAttribute("userName", loginParam);
+            session.setAttribute("Admin", login);
+        }
+    }
+
+    private void doLogout(HttpServletRequest request) {
+        // On termine la session
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+
+    private String findUserInSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        return (session == null) ? null : (String) session.getAttribute("userName");
+    }
+
+    private String findAdminInSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        return (session == null) ? null : (String) session.getAttribute("Admin");
+    }
 }
