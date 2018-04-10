f05b348084ac4b4769dc1b4453cfe473d5fbe023
Quentin <Quentin@192.168.1.30>
2018-04-10 15:28:28 +0200

    quelques commentaires...

--- src/main/java/controllers/JsonSalesByCustomerController.java
+++ src/main/java/controllers/JsonSalesByCustomerController.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// Bibliothèque Google GSon
 import com.google.gson.*;
+
 import java.sql.Date;
 import java.sql.SQLException;
 import java.text.DateFormat;
@@ -22,96 +24,95 @@ import java.util.Properties;
 import modeles.DAO;
 import modeles.DataSourceFactory;
 
-@WebServlet(name = "salesByCustomer", urlPatterns = {"/salesByCustomer"})
+@WebServlet(name = "salesByCustomer", urlPatterns = {"/salesByCustomer"}) // L'URL de la Servlet
 public class JsonSalesByCustomerController extends HttpServlet {
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException, ParseException {
-		// Créér le DAO avec sa source de données
-		DAO dao = new DAO(DataSourceFactory.getDataSource());
-                
-                String datePickerDebut = request.getParameter("datePickerDebut");
-                String datePickerFin = request.getParameter("datePickerFin");
-                
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                Date dateDebut = new java.sql.Date(format.parse(datePickerDebut).getTime());
-                Date dateFin = new java.sql.Date(format.parse(datePickerFin).getTime());
-                
-		// Properties est une Map<clé, valeur> pratique pour générer du JSON
-		Properties resultat = new Properties();
-		try {
-			resultat.put("records", dao.salesByCustomer(dateDebut, dateFin));
-		} catch (SQLException ex) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			resultat.put("records", Collections.EMPTY_LIST);
-			resultat.put("message", ex.getMessage());
-		}
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.text.ParseException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ParseException {
+        
+        // Créér le DAO avec sa source de données
+        DAO dao = new DAO(DataSourceFactory.getDataSource());
 
-		try (PrintWriter out = response.getWriter()) {
-			// On spécifie que la servlet va générer du JSON
-			response.setContentType("application/json;charset=UTF-8");
-			// Générer du JSON
-			// Gson gson = new Gson();
-			// setPrettyPrinting pour que le JSON généré soit plus lisible
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			String gsonData = gson.toJson(resultat);
-			out.println(gsonData);
-		}
-	}
+        // On récupère les paramètres saisis par l'administrateur
+        String datePickerDebut = request.getParameter("datePickerDebut");
+        String datePickerFin = request.getParameter("datePickerFin");
 
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (ParseException ex) {
-                Logger.getLogger(JsonSalesByCustomerController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
+        // Conversion des dates au format SQL
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateDebut = new java.sql.Date(format.parse(datePickerDebut).getTime());
+        Date dateFin = new java.sql.Date(format.parse(datePickerFin).getTime());
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (ParseException ex) {
-                Logger.getLogger(JsonSalesByCustomerController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
+        // Properties est une Map<clé, valeur> pratique pour générer du JSON
+        Properties resultat = new Properties();
+        // Fabrique le tableau de données en exécutant une requête du DAO
+        try {
+            resultat.put("records", dao.salesByCustomer(dateDebut, dateFin));
+        } catch (SQLException ex) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resultat.put("records", Collections.EMPTY_LIST);
+            resultat.put("message", ex.getMessage());
+        }
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
+        try (PrintWriter out = response.getWriter()) {
+            // On spécifie que la servlet va générer du JSON
+            response.setContentType("application/json;charset=UTF-8");
+            // Générer du JSON
+            // setPrettyPrinting pour que le JSON généré soit plus lisible
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String gsonData = gson.toJson(resultat);
+            out.println(gsonData);
+        }
+    }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(JsonSalesByCustomerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(JsonSalesByCustomerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    } // </editor-fold>
 }
--- src/main/java/controllers/JsonUpdateCommandeController.java
+++ src/main/java/controllers/JsonUpdateCommandeController.java
@@ -1,10 +1,10 @@
 package controllers;
 
+// Bibliothèque Google GSon
 import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Date;
-import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,95 +18,92 @@ import modeles.DAO;
 import modeles.DAOException;
 import modeles.DataSourceFactory;
 
-/**
- *
- * @author rbastide
- */
-@WebServlet(name = "updateCommande", urlPatterns = {"/updateCommande"})
+@WebServlet(name = "updateCommande", urlPatterns = {"/updateCommande"}) // L'URL de la Servlet
 public class JsonUpdateCommandeController extends HttpServlet {
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException, DAOException {
-            
-		DAO dao = new DAO(DataSourceFactory.getDataSource());
-                
-                int newQte = Integer.parseInt(request.getParameter("newQte"));
-                int order_num = Integer.parseInt(request.getParameter("order_num"));
-                float shipping_cost = 400 + 2 * newQte;
-                String freight_company = request.getParameter("fc");
-                int product_id = Integer.parseInt(request.getParameter("product_id"));
-                int quantity = Integer.parseInt(request.getParameter("qte"));
-		String message;
-		
-		dao.updateCommande(newQte, freight_company, shipping_cost, order_num);
-                dao.updateQuantity(newQte - quantity, product_id);
-                message = String.format("Commande %s modifiée", order_num);
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws modeles.DAOException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, DAOException {
 
-		Properties resultat = new Properties();
-		resultat.put("message", message);
+        // Créér le DAO avec sa source de données
+        DAO dao = new DAO(DataSourceFactory.getDataSource());
 
-		try (PrintWriter out = response.getWriter()) {
-			// On spécifie que la servlet va générer du JSON
-			response.setContentType("application/json;charset=UTF-8");
-			// Générer du JSON
-			Gson gson = new Gson();
-			out.println(gson.toJson(resultat));
-		}
-	}
+        // On récupère les paramètres saisis par le client
+        int newQte = Integer.parseInt(request.getParameter("newQte"));
+        int order_num = Integer.parseInt(request.getParameter("order_num"));
+        String freight_company = request.getParameter("fc");
+        int product_id = Integer.parseInt(request.getParameter("product_id"));
+        int quantity = Integer.parseInt(request.getParameter("qte"));
+        
+        // On définit les autres variables par rapport aux éléments saisis
+        float shipping_cost = 400 + 2 * newQte;
+        String message;
+
+        // On exécute les requêtes du DAO
+        dao.updateCommande(newQte, freight_company, shipping_cost, order_num);
+        dao.updateQuantity(newQte - quantity, product_id);
+        message = String.format("Commande %s modifiée", order_num);
+
+        Properties resultat = new Properties();
+        resultat.put("message", message);
+
+        try (PrintWriter out = response.getWriter()) {
+            // On spécifie que la servlet va générer du JSON
+            response.setContentType("application/json;charset=UTF-8");
+            // Générer du JSON
+            Gson gson = new Gson();
+            out.println(gson.toJson(resultat));
+        }
+    }
 
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (DAOException ex) {
-                Logger.getLogger(JsonUpdateCommandeController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOException ex) {
+            Logger.getLogger(JsonUpdateCommandeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (DAOException ex) {
-                Logger.getLogger(JsonUpdateCommandeController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOException ex) {
+            Logger.getLogger(JsonUpdateCommandeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
-
+    /**
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    } // </editor-fold>
 }
